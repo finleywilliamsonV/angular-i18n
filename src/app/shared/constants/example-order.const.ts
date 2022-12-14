0828c6aab9d273adc864adfe20f3af966943965d
Finley Williamson V <finley.williamson@gmail.com>
2022-12-14 16:23:18 -0500

    Add translations for ngFor-looped entries

--- src/app/shared/constants/example-order.const.ts
+++ src/app/shared/constants/example-order.const.ts
@@ -1,28 +1,32 @@
 import { Order } from '../models/order.model';
 
+/**
+ * $localize `:{meaning}|{description}@@{custom_id}:string_to_translate`
+ */
+
 export const EXAMPLE_ORDER: Order = [
   {
     date: '12/14/22',
     amount: 12,
-    description: '2" Flat Brushes',
+    description: $localize`2" Flat Brushes`,
     price: 8.99,
   },
   {
     date: '12/14/22',
     amount: 4,
-    description: 'Pads of Watercolor Paper',
+    description: $localize`Pads of Watercolor Paper`,
     price: 23.49,
   },
   {
     date: '12/14/22',
     amount: 12,
-    description: 'Watercolor Palettes',
+    description: $localize`Watercolor Palettes`,
     price: 9.99,
   },
   {
     date: '12/14/22',
     amount: 2,
-    description: '12 Color Watercolor Paint Set',
+    description: $localize`12 Color Watercolor Paint Set`,
     price: 28.55,
   },
 ];
