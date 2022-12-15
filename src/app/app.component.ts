e286559b7c0dfdf546712f3a262726aa2747328c
Finley Williamson V <finley.williamson@gmail.com>
2022-12-15 13:11:22 -0500

    Clean up master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title: string = 'Hello and Welcome!';
-
-  constructor() {
-    // used for localization at runtime
-    this.title = this.localizeTitle();
-  }
-
-  /**
-   * Localizes a title string to the current locale
-   * @param newTitle the new title you would like to localize
-   * @returns the localized title
-   */
-  localizeTitle(newTitle: string = this.title): string {
-    return $localize`${newTitle}`;
-  }
+  constructor() {}
 }
